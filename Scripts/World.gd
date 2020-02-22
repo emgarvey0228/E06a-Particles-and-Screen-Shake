1c867a697f65df8eef678e6cf8a3aacae682cf35
Emily Garvey <54558528+emgarvey@users.noreply.github.com>
2020-02-21 20:46:44 -0500

    updated

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,10 +1,20 @@
+  
 extends Node2D
 
 onready var Camera = $Camera
-var trauma = .5
+export var trauma = 10
+
 
 func _ready():
-	set_process(true);
- 
+	pass
+
 func _physics_process(delta):
-	$Camera.add_trauma(.5) 
+	$Camera.add_trauma(10)
+	if Input.is_action_just_pressed("left"):
+		Camera.add_trauma(trauma)
+	if Input.is_action_pressed("right"):
+		Camera.add_trauma(trauma)
+	if Input.is_action_pressed("up"):
+		Camera.add_trauma(trauma)
+	if Input.is_action_pressed("down"):
+		Camera.add_trauma(trauma)
